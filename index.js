368ee5ff8e46eeaaa609aa5c535c6cf92d6286e4
Andrew Lee <andrew.lee@hired.com>
2019-09-11 20:50:08 -0700

    Implement signup route with bcrypt

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const models = require('./models');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ success: true });
+});
+
+app.post('/signup', (req, res) => {
+  // get username and password
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // hash password using bcrypt
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    // create new user with username and hashed password
+    models.User.create({ username, encryptedPassword: hash }).then(() => {
+      res.json({ success: true });
+    });
+  });
+});
+
+app.post('/login', (req, res) => {
+  // get username and password
+  // hash password
+  // compare hashed password with encrypted password in database
+  // if they are the same return token to user
+});
+
+app.get('/secret-route', (req, res) => {
+  // check for token in header
+  // if token is valid, then return the data
 });
 
 app.listen(3000);
--- migrations/20190912004421-create-user.js
+++ migrations/20190912004421-create-user.js
@@ -17,15 +17,17 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'created_at'
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'updated_at'
       }
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('users');
   }
 };
